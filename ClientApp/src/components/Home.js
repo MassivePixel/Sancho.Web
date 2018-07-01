f30bbcaac4e36fd36ba7d9fdf3df6b8d9c11a89f
Toni Petrina <tonipetrina@hotmail.com>
2018-07-01 11:00:54 +0200

    Refactoring echo client

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -3,11 +3,6 @@ import EchoPlugin from './plugins/echo';
 import { sanchoConnection } from '../infrastructure/sanchoConnection';
 
 class Home extends React.Component {
-  state = {
-    text: '',
-    messages: [],
-  };
-
   connection = new sanchoConnection();
 
   componentDidMount() {
@@ -19,44 +14,15 @@ class Home extends React.Component {
       <div className="container">
         <h1>SignalR</h1>
 
-        <div>
-          <input
-            type="text"
-            value={this.state.text}
-            onChange={e => this.setState({ text: e.target.value })}
-          />
-          <button onClick={this.send}>send</button>
-        </div>
-
-        {this.state.messages.map((m, i) => <li key={i}>{m}</li>)}
-
         <EchoPlugin connection={this.connection} />
       </div>
     );
   }
 
   connect = () => {
-    this.connection.addListener('test', message => {
-      this.log(`got message: ${message.command}: '${message.data}'`);
-    });
-
-    this.log('connecting...');
-
     this.connection.connect().then(() => {
-      this.log('connection started');
-      this.connection.send('test', 'server test!', 'test!');
+      // connected
     });
-  };
-
-  send = () => {
-    this.connection.send('test', 'chat', this.state.text);
-    this.setState({ text: '' });
-  };
-
-  log = msg => {
-    this.setState(s => ({
-      messages: [msg, ...s.messages],
-    }));
   };
 }
 export default Home;
--- ClientApp/src/components/plugins/echo.js
+++ ClientApp/src/components/plugins/echo.js
@@ -1,11 +1,50 @@
 import React from 'react';
 
 export default class EchoPlugin extends React.PureComponent {
+  state = {
+    text: '',
+    messages: [],
+  };
+
+  componentDidMount() {
+    this.props.connection.addListener('test', this._handleReceive);
+  }
+
+  componentWillUnmount() {
+    this.props.connection.removeListener('test', this._handleReceive);
+  }
+
   render() {
     return (
       <div className="plugin">
         <h3>Echo</h3>
+
+        <div>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={e => this.setState({ text: e.target.value })}
+          />
+          <button onClick={this.send}>send</button>
+        </div>
+
+        {this.state.messages.map((m, i) => <li key={i}>{m}</li>)}
       </div>
     );
   }
+
+  send = () => {
+    this.props.connection.send('test', 'chat', this.state.text);
+    this.setState({ text: '' });
+  };
+
+  _handleReceive = message => {
+    this._log(`got message: ${message.command}: '${message.data}'`);
+  };
+
+  _log = msg => {
+    this.setState(s => ({
+      messages: [msg, ...s.messages],
+    }));
+  };
 }
