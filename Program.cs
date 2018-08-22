9d5a34a56911dab2c95d26d2f85c59a2cf8ac0de
Toni Petrina <tonipetrina@hotmail.com>
2018-08-22 23:10:53 +0200

    Add support for running on open IP

--- Program.cs
+++ Program.cs
@@ -18,7 +18,9 @@ namespace Sancho.Web
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost
+                .CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:5000")
                 .UseStartup<Startup>();
     }
 }
